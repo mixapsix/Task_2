3a94975d8f2e16146febf7b182e158c2522fb643
mixapsix <mixapsix1@mail.ru>
2019-12-09 20:39:54 +0300

    Проверка

---
+++ Задание 2/FilesSeeker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace Задание_2
+{
+    class FilesSeeker
+    {
+        private string directory;
+        private string fileMask;
+        private string result;
+
+        public string Directory { get => directory; set => directory = value; }
+        public string FileMask { get => fileMask; set => fileMask = value; }
+        public string Result { get => result; set => result = value; }
+
+        public void FilesInDirectory()
+        {
+            string[] allFiles = null;
+            string filesDirectory = @"C:\" + Directory;
+            string matchFiles = "";
+            try
+            {
+                allFiles = System.IO.Directory.GetFiles(filesDirectory);
+                for(int  i = 0; i < allFiles.Length ;i++)
+                {
+                    allFiles[i] = allFiles[i].Remove(0, filesDirectory.Length);
+                }
+            }
+            catch(DirectoryNotFoundException)
+            {
+                MessageBox.Show("Директория не найдена!");
+            }
+
+            try
+            {
+                foreach (string file in allFiles)
+                {
+                    if (file.Contains(fileMask))
+                    {
+                        matchFiles += file + "\n";
+                    }
+                }
+                result = matchFiles;
+
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Files not found");
+            }
+        }
+    }
+}
---
+++ Задание 2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задание_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FilesSeeker fileSeeker = new FilesSeeker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DirectoryInput(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            fileSeeker.Directory = textBox.Text;
+        }
+
+        private void FileMask(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            fileSeeker.FileMask = textBox.Text;
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock textBlock = new TextBlock();
+            fileSeeker.FilesInDirectory();
+            MessageBox.Show(fileSeeker.Result);
+        }
+    }
+}
